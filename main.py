413bfbe19338e049654f6ccf17d1857c53e33cb3
Aleksey <mc_gra_dy@mail.ru>
2023-07-09 11:24:24 +0300

    добавил обработку

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+hostName = 'localhost'
+serverPort = 8080
 
 
-# Press the green button in the gutter to run the script.
+class MyServer(BaseHTTPRequestHandler):
+
+    def get_html_content(self):
+        with open('Album example for Bootstrap.html', encoding='utf-8') as page:
+            content = page.read()
+        return content
+
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        page_content = self.get_html_content()
+        print(query_components)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes(page_content, 'utf-8'))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print('Server started http:/%s:%s' % (hostName, serverPort))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print('Server stopped.')
